1c04e4ba63c2415c124073c71ef86667d02b2322
wmzy <1256573276@qq.com>
2019-06-27 14:25:27 +0800

    chore: storybook propTablesExclude

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { Row, Col, Icon, Button, Modal} from 'antd';
 
-addDecorator(withInfo);
+addDecorator(withInfo({propTablesExclude: [Row, Col, Icon, Button, Modal]}));
 
 const req = require.context('../stories', true, /\.js$/);
 
